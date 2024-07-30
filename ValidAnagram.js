088b5e49e8ae96425eaf7c435fae30bff0f7ee78
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-07-30 09:12:36 +0530

    ValidAnagram

---
+++ ValidAnagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  //Sort Split Join
+  const sortedS = s.split("").sort().join("");
+  const sortedT = t.split("").sort().join("");
+
+  return sortedS === sortedT;
+};
+
+//Test cases
+const s1 = "anagram",
+  t1 = "nagaram";
+const s2 = "rat",
+  t2 = "car";
+
+console.log(isAnagram(s1, t1));
+console.log(isAnagram(s2, t2));
